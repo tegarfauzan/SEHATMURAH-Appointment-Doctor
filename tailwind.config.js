b586e71567c408b7e45e72f224ad06567ca13375
tegarfauzan <tegarfauzanfirdaus@gmail.com>
2024-09-13 17:52:50 +0700

    perubahan folder

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./public/**/*.{html,js}"],
+    content: ["./doctor-search.html" ,"./public/**/*.{html,js}"],
     theme: {
         extend: {
             fontFamily: {
